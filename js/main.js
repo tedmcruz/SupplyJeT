482640f75937ee38b305e243d84a549735b9bfa7
tedmcruz <tedcruz@live.com>
2022-07-20 18:08:58 -0400

    Testing add js function

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// Commentary
+
+/* 
+Multiline
+Commentary
+*/
+
+/* 
+Notes
+Variables are defined by "let" and "var". These are modifiable.
+Constants are defined by "const". This is not modifiable.
+Use specific variable and constant names and use lowercase letter or camelCase while naming them. Don't use accents or spaces.
+Number: Numbers and functions
+Strings: Letters
+Use " ; " at the end of every line in JS, unless it is an "if" function.
+It is possibe to do mathematical formulas in JS without any aditional steps other then writng the function.
+If the add fuction is used on a string, it will concatenate the strings without spaces.
+To asigne a value to a variable, use " = " sign.
+*/
+
+alert("Este pagina es para compras y cotizaciones en SUPPLYJET");
+
