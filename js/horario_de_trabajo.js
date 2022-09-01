38daf14dd32d8514462b7e7389528a5bffa08cbf
tedmcruz <tedcruz@live.com>
2022-08-31 22:08:31 -0400

    jsOptimization

--- js/horario_de_trabajo.js
+++ js/horario_de_trabajo.js
@@ -3,10 +3,10 @@ var diaHora = luxon.DateTime;
 var horaActual = diaHora.local();
 
 function refrescarHora() {
-if (diaHora.local()>=horaActual.startOf('day').plus({hours:8})&&diaHora.local()<=horaActual.endOf('day').minus({hours:6})){
+if (diaHora.local()>=horaActual.startOf('day').plus({hours:8,minutes:0})&&diaHora.local()<=horaActual.endOf('day').minus({hours:6,minutes:0})&&diaHora.local().weekday!=6&&diaHora.local().weekday!=7){
     reloj.innerHTML = "ESTAMOS LABORANDO! SON LAS "+diaHora.local().toFormat("hh:mm:ss a");
 } else {
-    reloj.innerHTML = "YA ESTAMOS CERRADOS! "+diaHora.local().toFormat("hh:mm:ss a");
+    reloj.innerHTML = "YA ESTAMOS CERRADOS! NUESTRO HORARIO ES DE 8AM A 6PM DE LUNES A VIERNES. SON LAS "+diaHora.local().toFormat("hh:mm:ss a");
 };
 };
 
