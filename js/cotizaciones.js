7cbde4d3aa4be52c754c4d75ae28c52a155ed90e
tedmcruz <tedcruz@live.com>
2022-09-26 00:41:19 -0400

    Fixing formula for cotizaciones

--- js/cotizaciones.js
+++ js/cotizaciones.js
@@ -148,36 +148,3 @@ function obtenerNumero() {
         localStorage.setItem("textoArticulo4",JSON.stringify(texto));
 
     };
-
-    var input = document.getElementById("numeroArticulo1");
-    input.addEventListener("keypress", function(event) {
-        if (event.key === "Enter") {
-            event.preventDefault();
-            document.getElementById("botonCotizacionesArticulos").onclick();
-        }
-    });
-    
-    input = document.getElementById("numeroArticulo2");
-    input.addEventListener("keypress", function(event) {
-        if (event.key === "Enter") {
-            event.preventDefault();
-            document.getElementById("botonCotizacionesArticulos").onclick();
-        }
-    });
-
-    input = document.getElementById("numeroArticulo3");
-    input.addEventListener("keypress", function(event) {
-        if (event.key === "Enter") {
-            event.preventDefault();
-            document.getElementById("botonCotizacionesArticulos").onclick();
-        }
-    });
-
-    input = document.getElementById("numeroArticulo4");
-    input.addEventListener("keypress", function(event) {
-        if (event.key === "Enter") {
-            event.preventDefault();
-            document.getElementById("botonCotizacionesArticulos").onclick();
-        }
-    console.log(datos.length);
-});
