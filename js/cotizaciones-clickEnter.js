013083e3369c03e42ebeb420febf95d31fc8b144
tedmcruz <tedcruz@live.com>
2022-09-26 01:23:24 -0400

    Fixing formula for cotizaciones

--- js/cotizaciones-clickEnter.js
+++ js/cotizaciones-clickEnter.js
@@ -1,31 +1,31 @@
-var input = document.getElementById("numeroArticulo1");
-    input.addEventListener("keypress", function(event) {
-        if (event.key === "Enter") {
-            event.preventDefault();
+var teclaEnter = document.getElementById("numeroArticulo1");
+    teclaEnter.addEventListener("keypress", function(eventoEnter) {
+        if (eventoEnter.key === "Enter") {
+            eventoEnter.preventDefault();
             document.getElementById("botonCotizacionesArticulos").onclick();
         }
     });
     
-    input = document.getElementById("numeroArticulo2");
-    input.addEventListener("keypress", function(event) {
-        if (event.key === "Enter") {
-            event.preventDefault();
+    teclaEnter = document.getElementById("numeroArticulo2");
+    teclaEnter.addEventListener("keypress", function(eventoEnter) {
+        if (eventoEnter.key === "Enter") {
+            eventoEnter.preventDefault();
             document.getElementById("botonCotizacionesArticulos").onclick();
         }
     });
 
-    input = document.getElementById("numeroArticulo3");
-    input.addEventListener("keypress", function(event) {
-        if (event.key === "Enter") {
-            event.preventDefault();
+    teclaEnter = document.getElementById("numeroArticulo3");
+    teclaEnter.addEventListener("keypress", function(eventoEnter) {
+        if (eventoEnter.key === "Enter") {
+            eventoEnter.preventDefault();
             document.getElementById("botonCotizacionesArticulos").onclick();
         }
     });
 
-    input = document.getElementById("numeroArticulo4");
-    input.addEventListener("keypress", function(event) {
-        if (event.key === "Enter") {
-            event.preventDefault();
+    teclaEnter = document.getElementById("numeroArticulo4");
+    teclaEnter.addEventListener("keypress", function(eventoEnter) {
+        if (eventoEnter.key === "Enter") {
+            eventoEnter.preventDefault();
             document.getElementById("botonCotizacionesArticulos").onclick();
         };
     });
