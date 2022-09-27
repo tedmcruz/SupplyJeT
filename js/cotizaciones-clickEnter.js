f03fe7c0f5ce079b8ecb59caf440fa08021c1326
tedmcruz <tedcruz@live.com>
2022-09-27 14:51:31 -0400

    Fixing automatic formula generation

--- js/cotizaciones-clickEnter.js
+++ js/cotizaciones-clickEnter.js
@@ -1,17 +1,19 @@
-// var presionarTeclaEnter; 
-//     presionarTeclaEnter= document.getElementById("numeroArticulo1");
-//     presionarTeclaEnter.addEventListener("keypress", eventoEnter);
-
 async function presionarTeclaEnter() {
     let teclaEnter;
-    // await recopilarDatos();
-    teclaEnter = document.getElementById("numeroArticulo1");
-    teclaEnter.addEventListener("keypress", function(eventoEnter) {
-        if (eventoEnter.key === "Enter") {
-            eventoEnter.preventDefault();
-            document.getElementById("botonCotizacionesArticulos").onclick();
-        }
-    });
+    await fetch('../js/datos.json')
+    .then( (res) => res.json())
+    .then ( (datos) => {
+        datos.forEach((producto) => {
+
+            teclaEnter = document.getElementById("numeroArticulo1");
+            teclaEnter.addEventListener("keypress", function(eventoEnter) {
+                if (eventoEnter.key === "Enter") {
+                    eventoEnter.preventDefault();
+                    document.getElementById("botonCotizacionesArticulos").onclick();
+                }
+            });
+        });
+    })
 };
 
 async function ejecutarCotizaciones() {
@@ -21,26 +23,3 @@ async function ejecutarCotizaciones() {
 };
 
 ejecutarCotizaciones();
-    // presionarTeclaEnter = document.getElementById("numeroArticulo2");
-    // presionarTeclaEnter.addEventListener("keypress", function(eventoEnter) {
-    //     if (eventoEnter.key === "Enter") {
-    //         eventoEnter.preventDefault();
-    //         document.getElementById("botonCotizacionesArticulos").onclick();
-    //     }
-    // });
-
-    // presionarTeclaEnter = document.getElementById("numeroArticulo3");
-    // presionarTeclaEnter.addEventListener("keypress", function(eventoEnter) {
-    //     if (eventoEnter.key === "Enter") {
-    //         eventoEnter.preventDefault();
-    //         document.getElementById("botonCotizacionesArticulos").onclick();
-    //     }
-    // });
-
-    // presionarTeclaEnter = document.getElementById("numeroArticulo4");
-    // presionarTeclaEnter.addEventListener("keypress", function(eventoEnter) {
-    //     if (eventoEnter.key === "Enter") {
-    //         eventoEnter.preventDefault();
-    //         document.getElementById("botonCotizacionesArticulos").onclick();
-    //     };
-    // });
