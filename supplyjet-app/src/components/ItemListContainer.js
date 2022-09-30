dce8e93d34f420ece1870215f1e3f22380ab1291
tedmcruz <tedcruz@live.com>
2022-09-30 04:02:52 -0400

    Adding Navbar and CartWidget

---
+++ supplyjet-app/src/components/ItemListContainer.js
@@ -0,0 +1,10 @@
+const ItemListContainer = () => {
+    return (
+        <div>
+            <h2 className="page__header">DEPARTAMENTOS</h2>
+
+        </div>
+    )
+}    
+
+export default ItemListContainer
