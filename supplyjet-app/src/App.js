f4ceec4014de1cb34f97da4189dd5d131292a8d2
tedmcruz <tedcruz@live.com>
2022-10-13 18:52:45 -0400

    Catalog and Product Details

--- supplyjet-app/src/App.js
+++ supplyjet-app/src/App.js
@@ -2,15 +2,19 @@ import './App.css';
 import './NavBar.css';
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
+import Contador from './components/Contador';
+import Productos from './components/Productos'
 
 function App() {
   return (
     <div>
       <header>
         <NavBar />
-        <ItemListContainer />
       </header>
+      <ItemListContainer />
+      <Contador/>
     </div>
+    
   );
 }
 
--- supplyjet-app/src/components/ItemListContainer.js
+++ supplyjet-app/src/components/ItemListContainer.js
@@ -1,8 +1,11 @@
+import React from "react";
+import ItemList from './ItemList'
+
 const ItemListContainer = () => {
     return (
         <div>
             <h2 className="page__header">DEPARTAMENTOS</h2>
-
+                <ItemList />
         </div>
     )
 }    
