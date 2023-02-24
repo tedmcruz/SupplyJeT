6cd34f611eed8282b376b164489a564be008a202
tedmcruz <tedcruz@live.com>
2023-02-23 23:22:32 -0400

    2023/02/23 Backup

--- js/productManager.js
+++ js/productManager.js
@@ -1,83 +1,123 @@
-const products = [
-];
+const fs = require("fs");
 
-const codeOfProducts = [
+class ProductManager {
+    #nextId = 0;
+    #path;
+    //  = "./Products.json"; //#path is used to make a private path that is not accessible outside of the class
 
-];
 
-function testConstructor() 
-{
-    const codeOfProductsLength = codeOfProducts.length;
-
-    let id = codeOfProductsLength+1;
-
-    let name = prompt("INTRODUZCA NOMBRE DEL ARTICULO");
-
-    let description = prompt("INTRODUZCA DESCRIPCION DE (DEL / DE LA) "+name+".");
-
-    let price = prompt("INTRODUZCA PRECIO DE (DEL / DE LA) "+name);
-
-    let thumbnail = "RUTA DE IMAGEN";
-
-    let code = prompt("INTRODUZCA CODIGO DE (DEL / DE LA) "+name+".");
-    while (code === 0 || codeOfProducts.includes(code)===true || code === undefined || code === NaN || code === null || code.trim() === "") {
-        if (codeOfProducts.includes(code)!=true && code != 0 && code != null && code != NaN && code != undefined && code.trim() != "") {
-            break;
-        };
-        alert("DIGITAR CODIGO NO REPETIDO");
-        code = prompt("INTRODUZCA CODIGO DE (DEL / DE LA) "+name+".");
-    };
-    let stock = prompt("INTRODUZCA CANTIDAD DISPONIBLE DE "+name+".");
-    
-    class productManager {
-
-        constructor (id, name, description, price, thumbnail, code, stock){
-            this.id = id;
-            this.name = name;
-            this.description = description;
-            this.price = parseFloat(price);
-            this.thumbnail = thumbnail;
-            this.code = code;
-            this.stock = parseInt(stock);
-        }
-        mostrar() {
-            console.log(nameOfProducts);
-        };
+    constructor(path){
+        this.#path = path
     }
 
-    const product = new productManager(id, name, description, price, thumbnail, code, stock);
-    products.push(product)
-    codeOfProducts.push(product.code)
-    console.log(products)
+    async getProducts() {
+    try {
+        const products = await fs.promises.readFile(this.#path,"utf-8");
+
+        return JSON.parse(products);
+    }   catch (emptyProductsFile) {
+        return [];
+    }
+    }
+
+    async getProductById(productId){
+        const products = await this.getProducts();
+
+        let searchedProduct = products.find(p => p.id === productId);
+
+        if (!searchedProduct) {
+            throw new Error(`Product with Product ID ${productId} doesn't exist.`)
+        }
+
+        console.log(searchedProduct);
+        // if (searchedProduct.id === id){
+        //     return searchedProduct = products.filter(searchedProduct => searchedProduct.id===searchedId)
+        // }
+    }
+
+    async addProduct(name, description, price, thumbnail, code, stock){
+        const products = await this.getProducts();
+
+        let id = products.length +1; // = this.#nextId
+
+        const newProduct = {
+        id,
+        name,
+        description,
+        price,
+        thumbnail,
+        code,
+        stock
+        };
+
+        const addedProducts = [...products, newProduct];
+
+        await fs.promises.writeFile(this.#path,JSON.stringify(addedProducts));
+
+        // this.#nextId += 1;
+    }
+    
+
+    async updateProduct(productId,keyToUpdate,dataToUpdate){ //newName, newDescription, newPrice, newThumbnail, newCode, newStock
+        const products = await this.getProducts();
+                
+        const updatedProduct = await products.map((p) => 
+            p.id === productId ? {...p, [keyToUpdate]:dataToUpdate } : p
+            );
+
+        await fs.promises.writeFile(this.#path,JSON.stringify(updatedProduct))
+
+        
+        // let productToModifyById = products.find(p => p.id === productId);
+
+        // if (!productToModifyById){
+        //     throw new Error(`Product ID ${productId} not found.`)
+        // }
+
+        // const updatedProduct = {
+        //     ...productToModifyById,
+        //     name : newName, 
+        //     description : newDescription, 
+        //     price : newPrice, 
+        //     thumbnail : newThumbnail, 
+        //     code : newCode, 
+        //     stock : newStock 
+        // };
+        console.log(updatedProduct);
+        
+    }
+
+    async deleteProduct(productId){
+        const products = await this.getProducts();
+        
+                
+        const updatedProduct = await products.map((p) => 
+            p.id === productId ? {...p, ["name"]:"", ["description"]:"",["price"]:"",["thumbnail"]:"",["code"]:"",["stock"]:""} : p
+            );
+
+        await fs.promises.writeFile(this.#path,JSON.stringify(updatedProduct))
+    }
 }
 
-function getProductById(){
-    let searchedProduct;
-    let code = prompt("INTRODUZCA CODIGO DEL ARTICULO DECEADO PARA LA BUSQUEDA.");
-    if (code === 0 || products.length === 0 || products.length === undefined ||codeOfProducts.includes(code)!=true || code === null || code === undefined) {
-        alert("CODIGO NO EXISTE");}
-    else if (codeOfProducts.includes(code)===true) {
-            searchedProduct = products.find(product => product.code === code);
-            console.log(searchedProduct);
-        };
-    console.log("El codigo introducido fue ="+code)
-    };
 
-function showProducts(){
-    console.log(products)
+async function main () {
+    const manager = new ProductManager("./Products.json");
+
+    let products = await manager.getProducts();
+
+    // console.log(products);
+
+    // await manager.addProduct("name", "description", 20, "thumbnail", 300, 4000);
+
+    // await manager.getProductById(2);
+
+    // await manager.updateProduct(1,"name","Mariposa"); //"Joyas", "Oro", 30, "thumbnail", 30, 2000
+
+    await manager.deleteProduct(3)
+
+    products = await manager.getProducts();
+
+    console.log(products);
 }
 
-function eliminateProductByCode(){
-    const filteredProducts = Array.from(products);
-    let code = prompt("INTRODUZCA CODIGO DEL ARTICULO QUE DESEA ELIMINAR.");
-    if (code === 0 || products.length === 0 || products.length === undefined ||codeOfProducts.includes(code)!=true || code === null || code === undefined) {
-        alert("CODIGO NO EXISTE");}
-    else if (codeOfProducts.includes(code)===true) {
-            eliminateProduct = filteredProducts.find(product => product.code === code);
-            console.log(eliminateProduct);
-        };
-    console.log("El codigo introducido fue ="+code)
-    const codeOfProductoToRemove = filteredProducts.findIndex((product) => product.code === code);
-        filteredProducts.splice(codeOfProductoToRemove, code);
-        console.log(filteredProducts);
-    };
+main();
